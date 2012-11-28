6b115216d635bb5d8140599d80102cc93538ce74
suisho <suisho000000@gmail.com>
2012-11-29 01:31:06 +0900

    revert old cssinclude

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ var utils = require("../lib/util.js");
 
 console.log("test start");
 
-utils.getReplaceMap("./test/input/dir/replace_map.css",[],function(err,result){
+utils.getReplaceMapByFile("./test/input/dir/replace_map.css",[],function(err,result){
   var expect = {
     '@import "parts.css";' : "./test/input/dir/parts.css",
     '@import "../base.css";' : "./test/input/base.css",
@@ -18,9 +18,9 @@ utils.getReplaceMap("./test/input/dir/replace_map.css",[],function(err,result){
     assert.equal(path.resolve(expect[key]),path.resolve(result[key]));
   }
 });
-/*
+
 index("./test/input/main.css",function(err,data){
   var output = fs.readFileSync("./test/output/main.css", 'utf-8');
   assert.equal(data,output);
 })
-*/
+
