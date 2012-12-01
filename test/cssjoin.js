cae7614fa7847ef77e71a58c25e54b1301ad9cf0
suisho <suisho000000@gmail.com>
2012-12-02 02:35:10 +0900

    Add test

--- test/cssjoin.js
+++ test/cssjoin.js
@@ -12,19 +12,29 @@ describe("old css-include",function(){
   })
 });
 describe("css-include2",function(){
-  
   var cssJoin = require("../lib/cssjoin.js");
-  
   it("Execute with no option",function(done){
-    cssJoin("./test/fixture/3/input/main.css",function(err,result){
+    cssJoin("./test/fixture/3/input/main.css", function(err,result){
       var expect = fs.readFileSync("./test/fixture/3/output/main.css",'utf-8');
       assert.equal(expect, result);
       done();
     });
   });
-  it("Execute with option")
+  it("Execute with option",function(done){
+    cssJoin("./test/fixture/3/input/main.css",{debug :true }, function(err,result){
+      var expect = fs.readFileSync("./test/fixture/3/output/main.css",'utf-8');
+      assert.equal(expect, result);
+      done();
+    });
+  });
   it("Resolve path test")
-  it("Can't resolve @import test")
+  it("Can't resolve @import test", function(done){
+    cssJoin("./test/fixture/5/input/main.css,",{debug :true }, function(err ,result){
+      var expect = fs.readFileSync("./test/fixture/5/output/main.css",'utf-8');
+      //assert.equal(expect, result);
+      done();
+    });
+  });
   it("Can't resolve @import test with error")  
   it("Create Map")
   it("Extend Test")
